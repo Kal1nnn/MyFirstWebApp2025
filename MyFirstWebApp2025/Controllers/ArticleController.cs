2ba2946093c303df0af483231fd94fcb7a657e2e
Kal1nnn <k.botev2021@pmg-pernik.com>
2025-11-25 12:09:30 +0200

    pusnat proekt

---
+++ MyFirstWebApp2025/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApp2025.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult ById()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
